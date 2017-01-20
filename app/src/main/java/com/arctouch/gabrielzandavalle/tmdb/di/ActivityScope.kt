021338d1dadd60367f2339ab1daefe29dedfafa0
Gabriel <gabriel.zandavalle@arctouch.com>
2017-01-20 14:22:58 -0200

    created separated file for ActivityScope annotation

---
+++ app/src/main/java/com/arctouch/gabrielzandavalle/tmdb/di/ActivityScope.kt
@@ -0,0 +1,13 @@
+package com.arctouch.gabrielzandavalle.tmdb.di
+
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import javax.inject.Scope
+
+/**
+ * Created by gabrielzandavalle on 1/20/17.
+ */
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+annotation class ActivityScope
--- app/src/main/java/com/arctouch/gabrielzandavalle/tmdb/di/MainActivityComponent.kt
+++ app/src/main/java/com/arctouch/gabrielzandavalle/tmdb/di/MainActivityComponent.kt
@@ -3,9 +3,6 @@ package com.arctouch.gabrielzandavalle.tmdb.di
 import com.arctouch.gabrielzandavalle.tmdb.HomeFragment
 import com.arctouch.gabrielzandavalle.tmdb.MainActivity
 import dagger.Subcomponent
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-import javax.inject.Scope
 
 /**
  * Created by gabrielzandavalle on 1/19/17.
@@ -18,7 +15,3 @@ interface MainActivityComponent{
   fun inject(activity: MainActivity)
   fun inject(activity: HomeFragment)
 }
-
-@Scope
-@Retention(RetentionPolicy.RUNTIME)
-annotation class ActivityScope
